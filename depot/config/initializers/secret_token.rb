9b1a52c674857c737d78193bd0add7dacd1adbf0
piyushanchanto <piyush.awasthi@anchanto.com>
2013-12-19 17:33:39 +0530

    Depot add to git

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '91e0eafcb777a40279c5c43812591afdbbd4398e8f09e81161827c32a05ffcf43028db0b5a0723f5896be1db08b5489f2ff2906fcb23e56a0520bea4ca6b44bf'
